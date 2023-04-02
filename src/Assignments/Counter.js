93edc8244878597f45a1410b68869d981cbdbf90
Anirban <meanirbandutta22@gmail.com>
2023-04-02 12:07:42 +0530

    Counter app ready

--- src/Assignments/Counter.js
+++ src/Assignments/Counter.js
@@ -1,5 +1,29 @@
-export default function Counter(){
-    return(
-        <h1>Hello</h1>
+import React,{useState} from "react";
+
+export default function Counter() {
+    const [count,setCount] = useState(0);
+
+    const countIncrement = (e)=>{
+        setCount(count+1);
+    }
+    const countDecrement = (e)=>{
+        setCount(count-1);
+    }
+    const countReset = (e)=>{
+        setCount(0);
+    }
+
+    let result = count==5?true:false;
+    let result2 = count==-5?true:false;
+    return (
+        <>
+            <h1 className="heading">Counter App</h1>
+            <div className="appArea">
+                <button className="btn" onClick={countIncrement} disabled={result}>+</button>
+                <h3>{count}</h3>
+                <button className="btn" onClick={countDecrement} disabled={result2}>-</button>
+                <button className="btn" onClick={countReset}>Reset</button>
+            </div>
+        </>
     );
 }
